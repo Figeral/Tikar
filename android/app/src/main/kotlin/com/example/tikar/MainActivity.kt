ace1b49015e170b55ba1374ef6ae989a46ef5df8
figeral <mouliofitzgerard@gmail.com>
2024-04-15 14:03:30 +0100

    project initialisation

---
+++ android/app/src/main/kotlin/com/example/tikar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tikar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
