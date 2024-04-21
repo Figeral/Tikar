81a2a0e28d7f5408b42e7953369ea87b9851146a
figeral <mouliofitzgerard@gmail.com>
2024-04-21 22:55:15 +0100

    Continuation of onboarding

--- lib/view/desktop/desktop.dart
+++ lib/view/desktop/desktop.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tikar/view/desktop/pages/landing/landing.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Desktop extends StatefulWidget {
   const Desktop({super.key});
@@ -8,10 +10,47 @@ class Desktop extends StatefulWidget {
 }
 
 class _DesktopState extends State<Desktop> {
+  final _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple.shade700,
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            children: [
+              Container(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: const Landing(),
+              ),
+              Container(
+                decoration: const BoxDecoration(color: Colors.red),
+              ),
+            ],
+          ),
+          GestureDetector(),
+          Container(),
+          Container(
+            alignment: const Alignment(0, 0.75),
+            child: SmoothPageIndicator(
+              controller: _pageController, count: 2,
+              onDotClicked: (int toPageIndex) {
+                if (toPageIndex.toInt() == 1) {
+                  _pageController.nextPage(
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeIn);
+                } else {
+                  _pageController.previousPage(
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeIn);
+                }
+              },
+              // axisDirection:,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
